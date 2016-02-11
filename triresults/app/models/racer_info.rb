39f9d250df001e240d42d82cdd8f05642d4fbf61
lestex <lestex@gmail.com>
2016-02-11 13:01:55 +0000

    week3 Racer and RacerInfo completed

--- triresults/app/models/racer_info.rb
+++ triresults/app/models/racer_info.rb
@@ -3,9 +3,16 @@ class RacerInfo
   field :racer_id, as: :_id
   field :_id, default:->{ racer_id }
   
-  field :first_name, as: :fn, type: String
-  field :last_name, as: :ln, type: String
-  field :gender, as: :g, type: String
-  field :birth_year, as: :yr, type: Integer
-  field :residence, as: :res, type: Address
+  field :fn, as: :first_name, type: String
+  field :ln, as: :last_name, type: String
+  field :g, as: :gender, type: String
+  field :yr, as: :birth_year, type: Integer
+  field :res, as: :residence, type: Address
+  
+  embedded_in :parent, polymorphic: true
+  
+  validates :first_name, :last_name, presence: true
+  validates :gender, presence: true, inclusion: { in: ['M', 'F'] }
+  validates :birth_year, presence: true,
+             numericality: { less_than: Date.today.year, message: 'must in past' }
 end
