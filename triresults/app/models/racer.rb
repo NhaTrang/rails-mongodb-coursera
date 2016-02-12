30ed33c336f6f64cf47663068da0c8275f3cbda4
lestex <lestex@gmail.com>
2016-02-12 23:51:58 +0000

    week3 flatten_attributes_spec.rb -e rq01

--- triresults/app/models/racer.rb
+++ triresults/app/models/racer.rb
@@ -4,6 +4,13 @@ class Racer
   embeds_one :info, autobuild: true, class_name: 'RacerInfo'
   has_many :races, class_name: 'Entrant', foreign_key: "racer.racer_id",
                   order: :"race.date".desc, dependent: :nullify
+        
+  delegate :first_name, :first_name=, to: :info
+  delegate :last_name, :last_name=, to: :info
+  delegate :gender, :gender=, to: :info
+  delegate :birth_year, :birth_year=, to: :info
+  delegate :city, :city=, to: :info
+  delegate :state, :state=, to: :info
   
   before_create do |racer|
     racer.info.id = racer.id
