8259956f2b08a7e5fabf4894d92256a924d55917
lestex <lestex@gmail.com>
2016-02-18 16:20:50 +0000

    week4 rspec spec/resources_spec.rb -e rq02

--- week4/app/controllers/api/results_controller.rb
+++ week4/app/controllers/api/results_controller.rb
@@ -1,6 +1,5 @@
 module Api
   class ResultsController < ApiController
-    before_action :set_objects, only: :show
     
     def index
       if !request.accept || request.accept == "*/*"
@@ -13,14 +12,10 @@ module Api
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
       else
-        # implementation
+        @result = Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
+        Rails.logger.debug("result ===== #{@result._id}")
+        render @result
       end
-    end
-    
-    private
-    def set_objects
-      @race = Race.find(params[:race_id])
-      @results = Race.find(params[:id])
     end
   end
 end
